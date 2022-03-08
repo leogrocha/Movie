fd4888d788f4b96142d66d620bee99a0af6a2194
Leonardo Rocha <leonardoleo182@hotmail.com>
2022-03-08 16:31:20 -0300

    MovieCard

---
+++ frontend/src/components/MovieScore/index.tsx
@@ -0,0 +1,23 @@
+//import { useState } from "react";
+import MovieStars from "../MovieStars";
+import './styles.css';
+
+
+const MovieScore = () => {
+    
+    // const [count, setCount] = useState(3.5);
+    // const [score, setScore] = useState(13);
+    const score = 3.5;
+    const count = 13;
+    
+    
+    return (
+        <div className="dsmovie-score-container">
+        <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
+        <MovieStars />
+        <p className="dsmovie-score-count">{count} avaliações</p>
+    </div>
+    )
+}
+
+export default MovieScore;
