2874260b6ee0912479071942388cb296857a86cd
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-03-12 17:08:04 -0300

    Save Score

--- backend/src/main/java/com/example/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/example/dsmovie/services/MovieService.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Service
 public class MovieService {
