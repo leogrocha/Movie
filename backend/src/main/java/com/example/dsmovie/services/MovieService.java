96505b12ed7bafd8194eb2f785d762b2437c8c8f
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2022-03-12 16:26:38 -0300

    Find movies

--- backend/src/main/java/com/example/dsmovie/services/MovieService.java
+++ backend/src/main/java/com/example/dsmovie/services/MovieService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class MovieService {
@@ -22,5 +23,13 @@ public class MovieService {
         Page<Movie> result = repository.findAll(Pageable);
         Page<MovieDTO> page = result.map(x -> new MovieDTO(x));
         return page;
+    }
+
+    @Transactional(readOnly = true)
+    public MovieDTO findById(Long id) {
+        Movie result = repository.findById(id).get();
+        MovieDTO dto = new MovieDTO(result);
+
+        return dto;
     }
 }
